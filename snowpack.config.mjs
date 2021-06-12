ddc50255b0c6e09bc2e1f09418803b42b792c32f
Nick Friday <nfriday@yandex.ru>
2021-06-13 00:14:27 +0300

    rm: testing
    muh smol site doesn't need tests, docs will be enough

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -11,26 +11,8 @@ export default {
     [
       '@snowpack/plugin-typescript',
       {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
         ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
       },
     ],
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
 };
